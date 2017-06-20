cbbe505c8f62605bb3dc8118066c25e655876429
ddjoker <1986jingdie@163.com>
2017-06-20 22:37:11 +0800

    spring boot error page mvcconfig

---
+++ src/main/java/com/ddjoker/ddjframe/support/config/APIMVCConfig.java
@@ -0,0 +1,24 @@
+package com.ddjoker.ddjframe.support.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by dong on 2017/6/20.
+ */
+@Configuration
+public class APIMVCConfig {
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurerAdapter() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**");
+      }
+    };
+  }
+}
